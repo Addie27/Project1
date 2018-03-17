98b3e540286fc3876f22e52f795a95d4887f4eaa
Julian T Skodzinski <juliantsk@gmail.com>
2018-03-17 13:32:30 -0400

    Got the EventBrite API working.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,4 +7,29 @@
      storageBucket: "project1-7d587.appspot.com",
      messagingSenderId: "843607985029"
  };
+
+ var eventbriteEvents;
+ var scienceEvents;
+ var techEvents;
+ var engineeringEvents;
+ var mathEvents;
+
  firebase.initializeApp(config);
+
+ // AJAX Call for Eventbrite
+ const phlSettings = {
+     url: 'https://www.eventbriteapi.com/v3/events/search/',
+     data: { token: 'NDTQNEDOWJ64E2QLDX2O', sort_by: 'date', 'location.address': 'Philadelphia', categories: "102,120", include_all_series_instances: "on", formats: "1,2,3,9,100" },
+     crossDomain: true,
+     method: 'GET'
+ }
+
+ $.ajax(phlSettings).done(function(eventObject) {
+     // All SF Area Events (Paginated by 50. Will only return first page.)
+     eventbriteEvents = eventObject.events;
+     // Create a new array of events whose venue is specifically in SF
+     console.log(eventbriteEvents);
+     //  const phlEvents = events.filter(function(event) {
+     //      return event.venue.address.city === 'Philadelphia';
+     //  });
+ });
