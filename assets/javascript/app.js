231c332aa514926aa75de22db4585f14a1fec1ab
Julian T Skodzinski <juliantsk@gmail.com>
2018-03-17 10:29:02 -0400

    Initiated files.

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+     apiKey: "AIzaSyCotD26tDnhFlwzBrUMUP8IU0L3CPESG5E",
+     authDomain: "project1-7d587.firebaseapp.com",
+     databaseURL: "https://project1-7d587.firebaseio.com",
+     projectId: "project1-7d587",
+     storageBucket: "project1-7d587.appspot.com",
+     messagingSenderId: "843607985029"
+ };
+ firebase.initializeApp(config);
