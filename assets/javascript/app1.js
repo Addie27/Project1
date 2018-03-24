f660bdf4d299cc277d7226e961221818c626c2e2
Adeline Johnson <groblewski.adeline@gmail.com>
2018-03-24 12:30:15 -0400

    changes to css

--- assets/javascript/app1.js
+++ assets/javascript/app1.js
@@ -15,11 +15,11 @@ $.ajax({
             var courseDiv = $("<div class='course'>");
             var p = $("<p>");
 
-            courseDiv.append(p.text(results[i].title))
-            courseDiv.append(p.text(results[i].homepage))
-            courseDiv.append(p.text(results[i].summary))
-            courseDiv.append(p.text(results[i].level))
-            courseDiv.append(p.text(results[i].expected_duration + " " + results[i].expected_duration_unit))
+            courseDiv.append(p.text(results[i].title)); 
+
+            // courseDiv.append(p.text(results[i].title) + " " + p.text(results[i].homepage) + " " + p.text(results[i].summary) + " " + p.text(results[i].level) + " " + p.text(results[i].expected_duration) + " " + results[i].expected_duration_unit)
+
+            $("#udacityCourses").prepend(courseDiv);
 
         };//for loop close
 });//ajax call end
