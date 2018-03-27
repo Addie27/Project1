462c00bb2b92e4b86ed874ccf8c321e221023466
Adeline Johnson <groblewski.adeline@gmail.com>
2018-03-26 20:13:46 -0400

    updated udacity api

--- assets/javascript/app1.js
+++ assets/javascript/app1.js
@@ -1,7 +1,9 @@
 
 var queryURL = "https://www.udacity.com/public-api/v0/courses";
 var courseInfo;
-var array = [];
+var title;
+var link;
+var summary; 
 
 $.ajax({
     url: queryURL,
@@ -14,41 +16,37 @@ $.ajax({
     console.log(results[1].title);
     console.log(results[2].title);
 
+   for (var i = 0; i < results.length; i++) {
+
+        var courseDiv = $("<div class='course'>");
+        var courseInfo = $("<a target='_blank'><br>");
+        var p = $("<p>");
+
+        
+        var link = results[i].homepage;
+        var summary = results[i].summary;
+
+        if (results[i].title.includes("Software") || results[i].title.includes("Programming") || results[i].title.includes("App")) {
+            var title = results[i].title;
+            console.log(title)
+
+            courseInfo.attr({
+                "href": link,
+            });
     
-    var searchField = "title";
-    var searchVal = "programming";
-    for (var i = 0; i < results.length; i++) {
-        if (results[i][searchField] == searchVal) {
-            array.push(results[i]);
-            console.log(array); 
+            courseInfo
+                .append(p.text("Title: " + title));
+    
+    
+            courseDiv.append(courseInfo);
+    
+            $("#udacityCourses").prepend(courseDiv);
         }
-    }
-
-    // for (var i = 0; i < results.length; i++) {
-
-    //     var courseDiv = $("<div class='course'>");
-    //     var courseInfo = $("<a target='_blank'><br>");
-    //     var p = $("<p>");
-
-    //     var title = results[i].title;
-    //     var link = results[i].homepage;
-    //     var summary = results[i].summary;
-
-    //     courseInfo.attr({
-    //         "href": link,
-    //     });
-
-    //     courseInfo
-    //         .append(p.text("Title: " + title));
 
 
-    //     courseDiv.append(courseInfo);
-
-    //     $("#udacityCourses").prepend(courseDiv);
-
-    // };//for loop close
+    };//for loop close
 
 });//ajax call end
 
 
-
+   
